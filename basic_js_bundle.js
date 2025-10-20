dcb5eef6aa12394097a652b57333cdfac8959985
Hokubanso <unalitkina@gmail.com>
2025-10-20 17:35:36 +0700

    Add files via upload

--- basic_js_bundle.js
+++ basic_js_bundle.js
@@ -2,6 +2,6 @@
     for (let i = 0; i <= btn_all.length; i++)
         {
             let btn_s_temp = getComputedStyle(btn_all[i]);
-            btn_all[i].style.color = btn_s_temp.color;
+            
             btn_all[i].style.borderColor = btn_s_temp.color
         }
