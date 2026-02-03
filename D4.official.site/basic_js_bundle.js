e3019d6f8c57ad65adbdd029be43c1718b153d5e
Hokubanso <unalitkina@gmail.com>
2026-02-03 21:55:15 +0700

    Add files via upload

---
+++ D4.official.site/basic_js_bundle.js
@@ -0,0 +1,60 @@
+    import character1 from './Data/characters.json';
+
+    alert(character1.name);
+    
+    let lates = document.getElementsByClassName('title');
+
+    let characters_pics = ["red","blue","green","orange"];
+    
+    let news_pics = ["red","blue","green","orange"];
+
+    let actual_pic_chars = 0;
+    let actual_pic_news = 0;
+    let actual_pic = 0
+
+    document.getElementById('carousel-pic-char').style.background = characters_pics[actual_pic];
+    document.getElementById('carousel-pic-news').style.background = news_pics[actual_pic];
+
+    function car_is_chars()
+    {
+        carousel_items = characters_pics
+        true_item = document.getElementById('carousel-pic-char')
+        shadow_item = document.getElementById('carousel-pic-char-shadow')
+        actual_pic = actual_pic_chars
+    }
+
+    function car_is_news()
+    {
+        carousel_items = news_pics
+        true_item = document.getElementById('carousel-pic-news')
+        shadow_item = document.getElementById('carousel-pic-news-shadow')
+        actual_pic = actual_pic_news
+    }
+
+    function carousel_forward()
+    {
+        actual_pic = actual_pic + 1
+        if (actual_pic == carousel_items.length)
+        {
+            actual_pic = 0
+        }
+        true_item.style.background = carousel_items[actual_pic]
+        if (true_item == document.getElementById('carousel-pic-char'))
+        {actual_pic_chars = actual_pic}
+        if (true_item == document.getElementById('carousel-pic-news'))
+        {actual_pic_news = actual_pic}
+    }
+
+    function carousel_backward()
+    {
+        actual_pic = actual_pic - 1
+        if (actual_pic < 0)
+        {
+            actual_pic = carousel_items.length - 1
+        }
+        true_item.style.background = carousel_items[actual_pic]
+        if (true_item == document.getElementById('carousel-pic-char'))
+        {actual_pic_chars = actual_pic}
+        if (true_item == document.getElementById('carousel-pic-news'))
+        {actual_pic_news = actual_pic}
+    }
